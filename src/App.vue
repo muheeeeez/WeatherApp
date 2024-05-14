271be4fcd4a19a76f0f462bd3b306de3be8ae2d5
Abdul-muiz Olaleye <olal0006@algonquinlive.com>
2024-05-14 00:27:48 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div><input type="text" name="" id="" /></div>
+    <div>Lagos, NG</div>
+    <div>
+      <img src="./images/Cloud.png" alt="" />
+    </div>
+    <div>
+      <h1>24.95◦C</h1>
+      <p>Overcast clouds</p>
+    </div>
+    <div>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
